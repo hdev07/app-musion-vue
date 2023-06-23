c7aa5f92ea95c57fb07451bd2f3113bc99acfa66
Alejandro Cruz <hcruz0716@gmail.com>
2023-06-22 20:01:12 -0600

    Add rutes

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundLayout from '../layouts/NotFoundLayout.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('../views/RegisterView.vue')
+  },
+  // Ruta comodín para capturar todas las URLs no encontradas
+  {
+    path: '/:catchAll(.*)',
+    component: NotFoundLayout
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/tailwind',
-      name: 'tailwind',
-      component: () => import('../views/TailwindView.vue')
-    },
-    {
-      path: '/vuetify',
-      name: 'vuetify',
-      // división de código a nivel de ruta
-      // esto genera un fragmento separado (About.[hash].js) para esta ruta
-      // que se carga de forma diferida cuando se visita la ruta.
-      component: () => import('../views/VuetifyView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
