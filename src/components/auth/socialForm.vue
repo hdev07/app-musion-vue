52035c06605f622b391a1c2efd0d7eafd7f412cf
Alejandro Cruz <hcruz0716@gmail.com>
2023-06-22 20:02:47 -0600

    Add social Form

---
+++ src/components/auth/socialForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+import GoogleImage from '@/assets/icons/Google.svg'
+import FacebookImage from '@/assets/icons/Facebook.svg'
+import AppleImage from '@/assets/icons/Apple.svg'
+
+const showAlert = ref(false)
+
+function googleLogin() {
+  showAlert.value = true
+}
+function fbLogin() {
+  showAlert.value = true
+}
+function appleLogin() {
+  showAlert.value = true
+}
+</script>
+<template>
+  <div class="flex justify-around m-4 px-12">
+    <v-btn @click="googleLogin" icon class="border" size="large">
+      <img :src="GoogleImage" alt="Logo-Google" class="w-8" />
+    </v-btn>
+    <v-btn @click="fbLogin" icon class="border" size="large">
+      <img :src="FacebookImage" alt="Logo-Facebook" class="w-8" />
+    </v-btn>
+    <v-btn @click="appleLogin" icon class="border" size="large">
+      <img :src="AppleImage" alt="Logo-Apple" class="w-8" />
+    </v-btn>
+  </div>
+</template>
+
+<style></style>
