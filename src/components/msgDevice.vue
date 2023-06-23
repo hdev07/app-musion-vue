376fd08c78927e76f0cb88755bb8669dc82235c0
Alejandro Cruz <hcruz0716@gmail.com>
2023-06-22 20:03:12 -0600

    Add message device

---
+++ src/components/msgDevice.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+<template>
+  <div
+    class="fixed top-0 left-0 w-full h-full flex justify-center items-center"
+  >
+    <div
+      class="bg-secondary text-white w-3/4 mx-auto p-5 rounded-md text-center"
+    >
+      <p class="text-3xl mb-2 font-extrabold">🚧⛔⚠️ ¡Alto! ⚠️⛔🚧</p>
+      <p class="text-2xl font-medium">
+        Este sitio web esta desarrollado para verlo desde un dispositivo movil.
+      </p>
+      <p class="text-md font-extralight mt-2">
+        Ingresa desde un dispositivo movil.
+      </p>
+    </div>
+  </div>
+</template>
