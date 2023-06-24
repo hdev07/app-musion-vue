886b8a50606f6976bb4b0ca3487d6ee54138041d
Alejandro Cruz <hcruz0716@gmail.com>
2023-06-24 16:45:22 -0600

    Add other routes

---
+++ src/layouts/NavbarLayout.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="fixed bottom-0 w-full bg-secondary pb-3 mt-3 z-50">
+    <div class="flex items-center w-">
+      <RouterLink to="/news" class="w-1/5">
+        <div
+          class="mx-1"
+          :class="{
+            'text-primary border-t-2 border-primary': activeTab === 'news',
+            'text-white': activeTab !== 'news'
+          }"
+          @click="changeTab('news')"
+        >
+          <div class="mt-3 flex justify-center">
+            <vue-feather stroke-width="1" type="grid" />
+          </div>
+          <p class="text-center mb-0 pt-1 font-light text-sm uppercase">
+            Inicio
+          </p>
+        </div>
+      </RouterLink>
+      <RouterLink to="/museums" class="w-1/5">
+        <div
+          class="mx-1"
+          :class="{
+            'text-primary border-t-2 border-primary': activeTab === 'list',
+            'text-white': activeTab !== 'list'
+          }"
+          @click="changeTab('list')"
+        >
+          <div class="mt-3 flex justify-center">
+            <vue-feather stroke-width="1" type="list" />
+          </div>
+          <p class="text-center mb-0 pt-1 font-light text-sm uppercase">
+            Lista
+          </p>
+        </div>
+      </RouterLink>
+      <RouterLink to="/map" class="w-1/5">
+        <div
+          class="mx-1"
+          :class="{
+            'text-primary border-t-2 border-primary': activeTab === 'map',
+            'text-white': activeTab !== 'map'
+          }"
+          @click="changeTab('map')"
+        >
+          <div class="mt-3 flex justify-center">
+            <vue-feather stroke-width="1" type="map-pin" />
+          </div>
+          <p class="text-center mb-0 pt-1 font-light text-sm uppercase">Mapa</p>
+        </div>
+      </RouterLink>
+      <RouterLink to="/favorites" class="w-1/5">
+        <div
+          class="mx-1"
+          :class="{
+            'text-primary border-t-2 border-primary': activeTab === 'fav',
+            'text-white': activeTab !== 'fav'
+          }"
+          @click="changeTab('fav')"
+        >
+          <div class="mt-3 flex justify-center">
+            <vue-feather stroke-width="1" type="heart" />
+          </div>
+          <p class="text-center mb-0 pt-1 font-light text-sm uppercase">Favs</p>
+        </div>
+      </RouterLink>
+      <RouterLink to="/profile" class="w-1/5">
+        <div
+          class="mx-1"
+          :class="{
+            'text-primary border-t-2 border-primary': activeTab === 'profile',
+            'text-white': activeTab !== 'profile'
+          }"
+          @click="changeTab('profile')"
+        >
+          <div class="mt-3 flex justify-center">
+            <vue-feather stroke-width="1" type="user" />
+          </div>
+          <p class="text-center mb-0 pt-1 font-light text-sm uppercase">
+            Perfil
+          </p>
+        </div>
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const activeTab = ref('news')
+const route = useRoute()
+
+// Actualizar la pestaña activa basada en la ruta actual
+activeTab.value = route.name || 'news'
+
+function changeTab(tab) {
+  activeTab.value = tab
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFoundLayout from '../layouts/NotFoundLayout.vue'
+import { useUserStore } from '../stores/authStore'
 
 const routes = [
   {
@@ -23,6 +24,40 @@ const routes = [
     name: 'walkthrough',
     component: () => import('../views/WalkthroughView.vue')
   },
+  {
+    path: '/',
+    component: () => import('../layouts/NavbarLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('../views/HomeView.vue')
+      },
+      {
+        path: '/news',
+        name: 'news',
+        component: () => import('../views/NewsView.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/museums',
+        name: 'museums',
+        component: () => import('../views/MuseumsView.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/favorites',
+        name: 'favorites',
+        component: () => import('../views/FavoritesView.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('../views/ProfileView.vue'),
+        meta: { requiresAuth: true }
+      }
+    ]
+  },
   // Ruta comodín para capturar todas las URLs no encontradas
   {
     path: '/:catchAll(.*)',
@@ -33,6 +68,27 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+})
+
+// Guarda de navegación para verificar la autenticación
+router.beforeEach(async (to, from, next) => {
+  const requiredAuth = to.meta.requiresAuth
+  const userStore = useUserStore()
+
+  // si existe el token en memoria
+  if (userStore.token) {
+    return next()
+  }
+
+  // si no existe el token (se refrescó el sitio web) v2
+  if (requiredAuth || sessionStorage.getItem('user')) {
+    await userStore.refreshToken()
+    if (userStore.token) {
+      return next()
+    }
+    return next('/signin')
+  }
+  return next()
 })
 
 export default router
