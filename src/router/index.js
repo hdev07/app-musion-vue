c87449abc8c24ce07a89d3e3bc2de2887bf639df
Alejandro Cruz <hcruz0716@gmail.com>
2023-06-27 14:04:55 -0600

    Changes in routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,65 +2,67 @@ import { createRouter, createWebHistory } from 'vue-router'
 import NotFoundLayout from '../layouts/NotFoundLayout.vue'
 import { useUserStore } from '../stores/authStore'
 
+const routes = [
+  {
+    path: '/',
+    component: () => import('../views/IndexView.vue')
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import('../views/RegisterView.vue')
+  },
+  {
+    path: '/walkthrough',
+    name: 'walkthrough',
+    component: () => import('../views/WalkthroughView.vue')
+  },
+  {
+    path: '/',
+    component: () => import('../layouts/NavbarLayout.vue'),
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import('../views/HomeView.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/museums',
+        name: 'museums',
+        component: () => import('../views/MuseumsView.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/favorites',
+        name: 'favorites',
+        component: () => import('../views/FavoritesView.vue'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('../views/ProfileView.vue'),
+        meta: { requiresAuth: true }
+      }
+    ]
+  },
+  // Ruta comodín para capturar todas las URLs no encontradas
+  {
+    path: '/:catchAll(.*)',
+    component: NotFoundLayout
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior: () => ({ left: 0, top: 0 }),
-  routes: [
-    ({
-      path: '/',
-      component: () => import('../views/IndexView.vue')
-    },
-    {
-      path: '/signin',
-      name: 'signin',
-      component: () => import('../views/LoginView.vue')
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: () => import('../views/RegisterView.vue')
-    },
-    {
-      path: '/walkthrough',
-      name: 'walkthrough',
-      component: () => import('../views/WalkthroughView.vue')
-    },
-    {
-      path: '/',
-      component: () => import('../layouts/NavbarLayout.vue'),
-      children: [
-        {
-          path: '/home',
-          name: 'home',
-          component: () => import('../views/HomeView.vue'),
-          meta: { requiresAuth: true }
-        },
-        {
-          path: '/museums',
-          name: 'museums',
-          component: () => import('../views/MuseumsView.vue'),
-          meta: { requiresAuth: true }
-        },
-        {
-          path: '/favorites',
-          name: 'favorites',
-          component: () => import('../views/FavoritesView.vue'),
-          meta: { requiresAuth: true }
-        },
-        {
-          path: '/profile',
-          name: 'profile',
-          component: () => import('../views/ProfileView.vue'),
-          meta: { requiresAuth: true }
-        }
-      ]
-    },
-    // Ruta comodín para capturar todas las URLs no encontradas
-    {
-      path: '/:catchAll(.*)',
-      component: NotFoundLayout
-    })
-  ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 })
 
 // Guarda de navegación para verificar la autenticación
