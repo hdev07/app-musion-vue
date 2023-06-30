e1025ea4101cc2392def582433960b8fae0a1931
Alejandro Cruz <hcruz0716@gmail.com>
2023-06-29 22:29:30 -0600

    Add pagination

---
+++ src/components/common/paginationCommon.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const currentTx = ref()
+const emit = defineEmits(['changePage'])
+
+watch(currentTx, (newVal) => {
+  emit('changePage', newVal)
+})
+
+defineProps({
+  perPage: { type: Number },
+  totalPages: { type: Number },
+  currentPage: { type: Number }
+})
+</script>
+<template>
+  <div class="flex justify-around">
+    <div class="absolute justify-center w-90">
+      <v-pagination
+        v-model="currentTx"
+        :length="totalPages"
+        active-color="primary"
+        size="x-small"
+        prev-icon="mdi-menu-left"
+        next-icon="mdi-menu-right"
+        class="my-4"
+        rounded="4"
+      />
+    </div>
+  </div>
+</template>
+
+<style></style>
