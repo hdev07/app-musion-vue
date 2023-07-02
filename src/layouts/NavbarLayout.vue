b8248674fd1cb5c91b7aac81a97a0e2b8ae143ce
Alejandro Cruz <hcruz0716@gmail.com>
2023-07-02 14:08:23 -0600

    Edit navbar Layout

--- src/layouts/NavbarLayout.vue
+++ src/layouts/NavbarLayout.vue
@@ -1,82 +1,85 @@
 <template>
-  <div class="fixed bottom-0 w-full bg-secondary pb-3 mt-3 z-50">
-    <div class="flex items-center w-">
-      <RouterLink to="/home" class="w-1/5">
-        <div
-          class="mx-1"
-          :class="{
-            'text-primary border-t-2 border-primary': activeTab === 'home',
-            'text-white': activeTab !== 'home'
-          }"
-          @click="changeTab('home')"
-        >
-          <div class="mt-3 flex justify-center">
-            <vue-feather stroke-width="1" type="grid" />
+  <div class="fixed bottom-0 w-full z-50">
+    <v-card class="mt-3 pb-3">
+      <div class="flex items-center w-full">
+        <RouterLink to="/home" class="w-1/5">
+          <div
+            class="mx-1"
+            :class="{
+              'text-primary border-t-2 border-primary': activeTab === 'home',
+              'text-white': activeTab !== 'home'
+            }"
+            @click="changeTab('home')"
+          >
+            <div class="mt-3 flex justify-center">
+              <vue-feather stroke-width="1" type="grid" />
+            </div>
+            <p class="text-center mb-0 pt-1 font-extralight text-xs">Inicio</p>
           </div>
-          <p class="text-center mb-0 pt-1 font-extralight text-xs">Inicio</p>
-        </div>
-      </RouterLink>
-      <RouterLink to="/museums" class="w-1/5">
-        <div
-          class="mx-1"
-          :class="{
-            'text-primary border-t-2 border-primary': activeTab === 'museums',
-            'text-white': activeTab !== 'museums'
-          }"
-          @click="changeTab('museums')"
-        >
-          <div class="mt-3 flex justify-center">
-            <vue-feather stroke-width="1" type="list" />
+        </RouterLink>
+        <RouterLink to="/museums" class="w-1/5">
+          <div
+            class="mx-1"
+            :class="{
+              'text-primary border-t-2 border-primary': activeTab === 'museums',
+              'text-white': activeTab !== 'museums'
+            }"
+            @click="changeTab('museums')"
+          >
+            <div class="mt-3 flex justify-center">
+              <vue-feather stroke-width="1" type="list" />
+            </div>
+            <p class="text-center mb-0 pt-1 font-extralight text-xs">Lista</p>
           </div>
-          <p class="text-center mb-0 pt-1 font-extralight text-xs">Lista</p>
-        </div>
-      </RouterLink>
-      <RouterLink to="/map" class="w-1/5">
-        <div
-          class="mx-1"
-          :class="{
-            'text-primary border-t-2 border-primary': activeTab === 'map',
-            'text-white': activeTab !== 'map'
-          }"
-          @click="changeTab('map')"
-        >
-          <div class="mt-3 flex justify-center">
-            <vue-feather stroke-width="1" type="map-pin" />
+        </RouterLink>
+        <RouterLink to="/map" class="w-1/5">
+          <div
+            class="mx-1"
+            :class="{
+              'text-primary border-t-2 border-primary': activeTab === 'map',
+              'text-white': activeTab !== 'map'
+            }"
+            @click="changeTab('map')"
+          >
+            <div class="mt-3 flex justify-center">
+              <vue-feather stroke-width="1" type="map-pin" />
+            </div>
+            <p class="text-center mb-0 pt-1 font-extralight text-xs">Mapa</p>
           </div>
-          <p class="text-center mb-0 pt-1 font-extralight text-xs">Mapa</p>
-        </div>
-      </RouterLink>
-      <RouterLink to="/favorites" class="w-1/5">
-        <div
-          class="mx-1"
-          :class="{
-            'text-primary border-t-2 border-primary': activeTab === 'favorites',
-            'text-white': activeTab !== 'favorites'
-          }"
-          @click="changeTab('favorites')"
-        >
-          <div class="mt-3 flex justify-center">
-            <vue-feather stroke-width="1" type="heart" />
+        </RouterLink>
+        <RouterLink to="/favorites" class="w-1/5">
+          <div
+            class="mx-1"
+            :class="{
+              'text-primary border-t-2 border-primary':
+                activeTab === 'favorites',
+              'text-white': activeTab !== 'favorites'
+            }"
+            @click="changeTab('favorites')"
+          >
+            <div class="mt-3 flex justify-center">
+              <vue-feather stroke-width="1" type="heart" />
+            </div>
+            <p class="text-center mb-0 pt-1 font-extralight text-xs">Favs</p>
           </div>
-          <p class="text-center mb-0 pt-1 font-extralight text-xs">Favs</p>
-        </div>
-      </RouterLink>
-      <RouterLink to="/profile" class="w-1/5">
-        <div
-          class="mx-1"
-          :class="{
-            'text-primary border-t-2 border-primary': activeTab === 'profile',
-            'text-white': activeTab !== 'profile'
-          }"
-          @click="changeTab('profile')"
-        >
-          <div class="mt-3 flex justify-center">
-            <vue-feather stroke-width="1" type="user" />
+        </RouterLink>
+        <RouterLink to="/profile" class="w-1/5">
+          <div
+            class="mx-1"
+            :class="{
+              'text-primary border-t-2 border-primary': activeTab === 'profile',
+              'text-white': activeTab !== 'profile'
+            }"
+            @click="changeTab('profile')"
+          >
+            <div class="mt-3 flex justify-center">
+              <vue-feather stroke-width="1" type="user" />
+            </div>
+            <p class="text-center mb-0 pt-1 font-extralight text-xs">Perfil</p>
           </div>
-          <p class="text-center mb-0 pt-1 font-extralight text-xs">Perfil</p>
-        </div>
-      </RouterLink>
-    </div>
+        </RouterLink>
+      </div>
+    </v-card>
   </div>
   <RouterView />
 </template>
