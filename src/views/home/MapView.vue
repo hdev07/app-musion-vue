4d96f7cba8e7987939203f7c51348b85e93ecfa4
Alejandro Cruz <hcruz0716@gmail.com>
2023-08-03 12:35:19 -0600

    Add markers and clusters in map

--- src/views/home/MapView.vue
+++ src/views/home/MapView.vue
@@ -11,6 +11,29 @@
     :maxZoom="mapConfig.maxZoom"
     :minZoom="mapConfig.minZoom"
   >
+    <MarkerCluster>
+      <div style="text-align: center">
+        <img
+          src="https://s3.amazonaws.com/musion.day/clusters/m1.png"
+          alt=""
+          width="30"
+          height="30"
+        />
+      </div>
+      <CustomMarker
+        v-for="(location, i) in locations"
+        :options="{ position: location }"
+        :key="i"
+      >
+        <div style="text-align: center">
+          <img
+            src="https://s3.amazonaws.com/musion.day/marker.png"
+            width="30"
+            height="30"
+          />
+        </div>
+      </CustomMarker>
+    </MarkerCluster>
     <CustomControl position="RIGHT_BOTTOM">
       <v-card class="mx-2 w-10 h-20">
         <vue-feather
@@ -42,7 +65,12 @@
 </template>
 
 <script setup>
-import { GoogleMap, CustomControl } from 'vue3-google-map'
+import {
+  GoogleMap,
+  CustomControl,
+  CustomMarker,
+  MarkerCluster
+} from 'vue3-google-map'
 import { ref, watch } from 'vue'
 
 let gmap = null
@@ -65,6 +93,32 @@ const mapConfig = {
     strictBounds: true
   }
 }
+
+const locations = [
+  { lat: -31.56391, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+  { lat: -34.671264, lng: 150.863657 },
+  { lat: -35.304724, lng: 148.662905 },
+  { lat: -36.817685, lng: 175.699196 },
+  { lat: -36.828611, lng: 175.790222 },
+  { lat: -37.75, lng: 145.116667 },
+  { lat: -37.759859, lng: 145.128708 },
+  { lat: -37.765015, lng: 145.133858 },
+  { lat: -37.770104, lng: 145.143299 },
+  { lat: -37.7737, lng: 145.145187 },
+  { lat: -37.774785, lng: 145.137978 },
+  { lat: -37.819616, lng: 144.968119 },
+  { lat: -38.330766, lng: 144.695692 },
+  { lat: -39.927193, lng: 175.053218 },
+  { lat: -41.330162, lng: 174.865694 },
+  { lat: -42.734358, lng: 147.439506 },
+  { lat: -42.734358, lng: 147.501315 },
+  { lat: -42.735258, lng: 147.438 },
+  { lat: -43.999792, lng: 170.463352 }
+]
 
 watch(
   () => mapRef.value?.ready,
